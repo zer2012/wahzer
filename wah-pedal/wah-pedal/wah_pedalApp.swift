c4aecb56bc225492c5142fc4832e7e3be2631b5e
Arthur Callari <64812@etu.he2b.be>
2026-09-15 01:02:15 +0200

    add: initial project + gitignore

---
+++ wah-pedal/wah-pedal/wah_pedalApp.swift
@@ -0,0 +1,17 @@
+//
+//  wah_pedalApp.swift
+//  wah-pedal
+//
+//  Created by Arthur Callari on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct wah_pedalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
